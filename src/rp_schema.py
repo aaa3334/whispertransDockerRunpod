fe42ef1fab8726a48201ff69d612047889433881
aaa3334 <63257282+aaa3334@users.noreply.github.com>
2025-04-07 10:44:51 +0930

    Initial commit

---
+++ src/rp_schema.py
@@ -0,0 +1,26 @@
+INPUT_VALIDATIONS = {
+    'audio_file': {
+        'type': str,
+        'required': True
+    },
+    'language': {
+        'type': str,
+        'required': False,
+        'default': None
+    },
+    'batch_size': {
+        'type': int,
+        'required': False,
+        'default': 16
+    },
+    'temperature': {
+        'type': float,
+        'required': False,
+        'default': 0
+    },
+    'debug': {
+        'type': bool,
+        'required': False,
+        'default': False
+    }
+}
